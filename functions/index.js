90fe55b8ca7dec39ef1487932b4579ad62662199
srujanjill <srujanjilla15@gmail.com>
2020-08-28 23:24:57 +0530

    Initial firebase database firestorm setup - add and showing data

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,53 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello world");
+  response.send("Hello");
+});
+
+//gets data from the firestore
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+//adding our own data
+exports.createScream = functions.https.onRequest((req, res) => {
+  if(req.method !== 'POST'){
+    return res.status(400).json(error: "Method not allowed");
+  }
+  //getting the new data
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  // adding the data to database 
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: 'document ${doc.id} created successfully' });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.log(err);
+    });
 });
