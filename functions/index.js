5eb5d9810730aa52ba7676d646fb61bacb197d98
srujanjill <srujanjilla15@gmail.com>
2020-09-02 21:12:18 +0530

    Autherization midleware added that ckecks whether the posts we send are authrozed or not

--- functions/index.js
+++ functions/index.js
@@ -18,6 +18,7 @@ const firebase = require("firebase");
 firebase.initializeApp(firebaseConfig);
 
 const db = admin.firestore();
+db.settings({ ignoreUndefinedProperties: true })
 
 app.get("/screams", (req, res) => {
   db.collection("screams")
@@ -38,12 +39,42 @@ app.get("/screams", (req, res) => {
     .catch((err) => console.error(err));
 });
 
+//all the posts gets authentication cause wat ever we check and see has to autorized
+const FBAuth = (req,res,next) =>{
+  let idToken;
+  if(req.headers.authorization && req.headers.authorization.startsWith("Bearer ")){
+    idToken = req.headers.authorization.split("Bearer ")[1];
+} else {
+  console.error("No token found");
+  return res.status(403).json({error: "Unauthorized"});
+}
+
+admin.auth().verifyIdToken(idToken)
+.then(decodedToken => {
+  req.user = decodedToken;
+  console.log(decodedToken);
+  return db.collection("users")
+  .where("userId","==",req.user.uid)
+  .limit(1)
+  .get();
+})
+.then(data =>{
+  req.user.handle = data.docs[0].data().handle;
+  return next();
+})
+.catch(err =>{
+  console.error("Error while verifying token",err);
+  return res.status(403).json(err);
+})
+}
+
+//by the time you get to this code you will already been verified
 //adding our own data
-app.post("/scream", (req, res) => {
+app.post("/scream", FBAuth, (req, res) => {
   //getting the new data
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString(),
   };
   // adding the data to database
